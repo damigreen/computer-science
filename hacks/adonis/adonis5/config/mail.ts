a444401ef71db85aa6b8a7892f72da7cfba14b0c
Damilola Faseun <damilola.faseun@gmail.com>
2021-10-27 21:42:55 +0100

    Mail config adonis5

---
+++ hacks/adonis/adonis5/config/mail.ts
@@ -0,0 +1,65 @@
+/**
+ * Config source: https://git.io/JvgAf
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { MailConfig } from '@ioc:Adonis/Addons/Mail'
+
+const mailConfig: MailConfig & { sender: string }= {
+  /*
+  |--------------------------------------------------------------------------
+  | Default mailer
+  |--------------------------------------------------------------------------
+  |
+  | The following mailer will be used to send emails, when you don't specify
+  | a mailer
+  |
+  */
+  mailer: 'smtp',
+
+  sender: Env.get("MYSQL_FROM") as string,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Mailers
+  |--------------------------------------------------------------------------
+  |
+  | You can define or more mailers to send emails from your application. A
+  | single `driver` can be used to define multiple mailers with different
+  | config.
+  |
+  | For example: Postmark driver can be used to have different mailers for
+  | sending transactional and promotional emails
+  |
+  */
+  mailers: {
+    /*
+    |--------------------------------------------------------------------------
+    | Smtp
+    |--------------------------------------------------------------------------
+    |
+    | Uses SMTP protocol for sending email
+    |
+    */
+    smtp: {
+      driver: 'smtp',
+      host: Env.get('SMTP_HOST'),
+      port: Env.get('SMTP_PORT'),
+      secure: false,
+			auth: {
+				user: Env.get('SMTP_USERNAME') as string,
+				pass: Env.get('SMTP_PASSWORD') as string,
+				type: 'login',
+			},
+      maxConnections: 5,
+      maxMessages: 100,
+      rateLimit: 10
+    },
+
+  },
+}
+
+export default mailConfig
