2699edd610d32527c799cb806875afb76ba835e6
Damilola Faseun <damilola.faseun@gmail.com>
2021-10-27 21:42:21 +0100

    javascript datastructures code and note

--- MD Files/JavaScript Data Structures and Algorithms/javascript_data_structures_and_algorithms.js
+++ MD Files/JavaScript Data Structures and Algorithms/javascript_data_structures_and_algorithms.js
@@ -221,3 +221,50 @@ function isPrime(n) {
 // console.log(isPrime(6))
 
 
+
+
+function primFactors(n) {
+  // n = 12 12/2=6 6/2=3 3/2=1r1
+  while(n % 2 == 0) {
+      console.log(2)
+      n = n / 2;
+  }
+
+  // n must be odd at this point. So we can skip one element
+  // (Note i = i +2)
+  for (var i = 3; i * i <= n; i = i +2) {
+      while(n % i == 0) {
+          console.log(i);
+          n = n / i;
+      }
+  }
+
+  // This condition is to handle the case when n is a prime number
+  // greater than 2
+  if (n > 2) console.log(n)
+}
+
+// console.log(primFactors(12));
+// primFactors(30)
+
+
+
+// > Prime numbers list
+// > 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
+// > All primes are of the form 6k+-1, wth the exception of 2 and 3.
+function isPrime(n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n%2 == 0 || n%3 == 0) return false;
+
+    for (var i = 5; i*i <= n; i+=6) {
+        if (n%i == 0  || n%(i+2) == 0) return false
+    }
+
+    return true;
+}
+// console.log(isPrime(101));
+
