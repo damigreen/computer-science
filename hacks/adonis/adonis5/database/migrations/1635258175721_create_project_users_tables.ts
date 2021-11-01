560af1201fbb336e6a34687f8787fea68930507f
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-01 21:04:47 +0100

    modified

--- hacks/adonis/adonis5/database/migrations/1635258175721_create_project_users_tables.ts
+++ hacks/adonis/adonis5/database/migrations/1635258175721_create_project_users_tables.ts
@@ -6,9 +6,9 @@ export default class ProjectUsers extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.integer('project_id').unsigned().references('id').inTable('projects')
-      table.integer('user_id').unsigned().references('id').inTable('users')
-      table.integer('sort_order').notNullable().defaultTo(0)
+      table.integer('project_id').notNullable().unsigned().references('id').inTable('projects')
+      table.integer('user_id').notNullable().unsigned().references('id').inTable('users')
+      table.integer('role_id').notNullable().unsigned().defaultTo(1)
       table.timestamps(true, true)
     })
   }
