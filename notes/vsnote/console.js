ca67dab4e598104535a16c85da5267ea10b4384a
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-01 22:05:21 +0100

    String functions

--- notes/vsnote/console.js
+++ notes/vsnote/console.js
@@ -1,2 +1,30 @@
-console.log(new Date(1635752448000));
-console.log(new Date(1638344448000));
+var str = "Jesus is King"
+
+const a = str.substring(9, 10)
+const b = str.indexOf('Jesus')
+const c = str.indexOf('Nothing')
+// console.log(b)
+// console.log(c)
+
+function existsInString(stringValue, search) {
+    return stringValue[search] !== -1
+};
+// console.log(existsInString(str, "King"))
+
+// Get the count  of a
+var str = "He's my king from this day until the last day";
+var count = 0;
+var pos = str.indexOf('a')
+while(pos !== -1) {
+    pos = str.indexOf('a', pos + 1);
+    count ++;
+}
+// console.log(count)
+
+// String Decomposition
+var test1 = "My head is blessed"
+console.log(test1.split('e'));
+
+// String Replace
+console.log(test1.replace("head", "life"));
+
