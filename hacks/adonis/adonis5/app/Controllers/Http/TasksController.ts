3f6582cb051d411e4d8f293587fe94b8562037c0
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-06 08:51:55 +0100

    Model and Database Query

--- hacks/adonis/adonis5/app/Controllers/Http/TasksController.ts
+++ hacks/adonis/adonis5/app/Controllers/Http/TasksController.ts
@@ -1,7 +1,19 @@
+import Status from 'Contracts/status';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Task from 'App/Models/Task';
 
 export default class TasksController {
   public async index ({}: HttpContextContract) {
+    // Database Module Query
+    // create SQL Query;
+    // SELECT * from tasks WHERE status_id = 1
+    const tasksDb = await Database.from("tasks").where("status_id", Status.IDLE).select("*");
+    
+    // Data Model Query
+    // create SQL Query;
+    // SELECT * from tasks WHERE status_id = 1
+    const tasksModel = await  Task.query().where('statusId', Status.IDLE).select("*");
   }
 
   public async create ({}: HttpContextContract) {
