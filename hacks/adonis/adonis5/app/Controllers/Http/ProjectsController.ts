2d51b26b0d985b8c394c653e7fd2178026e5c8fb
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-06 08:51:27 +0100

    Logic for show and index

--- hacks/adonis/adonis5/app/Controllers/Http/ProjectsController.ts
+++ hacks/adonis/adonis5/app/Controllers/Http/ProjectsController.ts
@@ -1,24 +1,50 @@
+import Status from 'Contracts/status';
+import Database from '@ioc:Adonis/Lucid/Database';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Project from 'App/Models/Project'
 
 export default class ProjectsController {
-  public async index ({}: HttpContextContract) {
+  public async index({ response }: HttpContextContract) {
+    const projects = await Project.query().where("statusId", Status.IDLE)
+
+    return response.json({ projects })
   }
 
-  public async create ({}: HttpContextContract) {
+  public async create({ }: HttpContextContract) {
   }
 
-  public async store ({}: HttpContextContract) {
+  public async store({ request, response }: HttpContextContract) {
+    // const name = request.input('name')
+    // const description = request.input("description")
+    const name = request.only(['name', "description"])
+
+    // const project = new Project();
+    // project.name = "Model Example 1";
+    // await project.save();
+
+    // const project = await Project.create({ name: name });
+
+    const project = await Database.insertQuery().table("projects").insert({ name })
+    // const project = await Database.insertQuery().table("projects").multiInsert([ name ])
+
+    return response.json({ project })
+    // return  response.json({ name, description })
+    // return  response.json({ name });
   }
 
-  public async show ({}: HttpContextContract) {
+  public async show({ params, response }: HttpContextContract) {
+    const projects = await Project.query().where("id", params.id).firstOrFail();
+    // const projects = await Project.query().where("id", params.id).first()
+
+    return response.json({ projects })
   }
 
-  public async edit ({}: HttpContextContract) {
+  public async edit({ }: HttpContextContract) {
   }
 
-  public async update ({}: HttpContextContract) {
+  public async update({ }: HttpContextContract) {
   }
 
-  public async destroy ({}: HttpContextContract) {
+  public async destroy({ }: HttpContextContract) {
   }
 }
