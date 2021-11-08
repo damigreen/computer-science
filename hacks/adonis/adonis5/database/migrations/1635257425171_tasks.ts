d9377f9a48d4ab3710bd033549438ba3a34d806f
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-08 10:48:53 +0100

    Removed comma

--- hacks/adonis/adonis5/database/migrations/1635257425171_tasks.ts
+++ hacks/adonis/adonis5/database/migrations/1635257425171_tasks.ts
@@ -3,20 +3,20 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class Tasks extends BaseSchema {
   protected tableName = 'tasks'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('name').notNullable()
       // table.text('descrption').notNullable()
       table.timestamp('due_at').nullable()
       table.integer('status_id').unsigned().notNullable().defaultTo(1)
-      table.integer('created_by').notNullable().unsigned().references('id').inTable('users'),
+      table.integer('created_by').notNullable().unsigned().references('id').inTable('users')
       table.integer('assigned_to').nullable().unsigned().references('id').inTable('users')
       table.timestamps(true, true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
