42c11e668e29960db65f2881749cda735abee736
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-08 10:48:19 +0100

    added password field to user creation

--- hacks/adonis/adonis5/database/migrations/1635256300009_create_users_tables.ts
+++ hacks/adonis/adonis5/database/migrations/1635256300009_create_users_tables.ts
@@ -6,8 +6,9 @@ export default class Users extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('username').unique().notNullable()
+      table.string('username', 50).unique().notNullable()
       table.string('email', 255).unique().nullable()
+      table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
       table.timestamps(true, true);
     });
