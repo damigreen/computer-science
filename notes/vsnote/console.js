85c1b3f878f9d282df4fc76969cd9a1af507ece7
Damilola Faseun <damilola.faseun@gmail.com>
2021-11-11 08:50:21 +0100

    Find sum implementation

--- notes/vsnote/console.js
+++ notes/vsnote/console.js
@@ -2,106 +2,108 @@ var DICTIONARY = "abcdefghijklmnopqrstuvwxyz0123456789".split("");
 // console.log(DICTIONARY)
 
 function encodeId(num) {
-    var base = DICTIONARY.length; //36
-    var encoded = "";
+  var base = DICTIONARY.length; //36
+  var encoded = "";
 
-    if (num == 0){
-        return DICTIONARY[0 ]
-    }
+  if (num == 0) {
+    return DICTIONARY[0];
+  }
 
-    while(num > 0) {
-        encoded += DICTIONARY[(num % base)]
-        num = Math.floor(num / base);
-    }
+  while (num > 0) {
+    encoded += DICTIONARY[num % base];
+    num = Math.floor(num / base);
+  }
 
-    // encoded = "9ujd88"
-    
-    // console.log(encoded);
-    return reverseWord(encoded)
+  // encoded = "9ujd88"
+
+  // console.log(encoded);
+  return reverseWord(encoded);
 }
 
 function reverseWord(str) {
-    var reversed = "";
-    for (var i = str.length - 1; i >= 0; i--) {
-        // reversed += str[i]
-        reversed += str.charAt(i)
-    }
-    return reversed
+  var reversed = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    // reversed += str[i]
+    reversed += str.charAt(i);
+  }
+  return reversed;
 }
 // console.log(encodeId(11231230));
 
 function decodeId(id) {
-    var base = DICTIONARY.length;
-    var decoded = "";
-    for (var i = 0; i < id.split("").length; i++) {
-        decoded = decoded * base + DICTIONARY.indexOf(id.charAt(i))
-        console.log('decoded',decoded, base, DICTIONARY.indexOf(id.charAt(i)))
-        // 6 36 6
-        // 240 36 24
-        // 8666 36 26
-        // 311978 36 2
-        // 11231230 36 22
-        // 11231230
-    }
-    return decoded
+  var base = DICTIONARY.length;
+  var decoded = "";
+  for (var i = 0; i < id.split("").length; i++) {
+    decoded = decoded * base + DICTIONARY.indexOf(id.charAt(i));
+    console.log("decoded", decoded, base, DICTIONARY.indexOf(id.charAt(i)));
+    // 6 36 6
+    // 240 36 24
+    // 8666 36 26
+    // 311978 36 2
+    // 11231230 36 22
+    // 11231230
+  }
+  return decoded;
 }
 
 // console.log(decodeId(encodeId(11231230)))
 
-var dict = 'ABCDEFG'
+var dict = "ABCDEFG";
 var num = 12;
-var base = '';
+var base = "";
 while (num > 0) {
-    base += dict[( num % 7)]
-    num = Math.floor(num / 7)
-    console.log(base, dict[( num % 7)], num)
+  base += dict[num % 7];
+  num = Math.floor(num / 7);
+  console.log(base, dict[num % 7], num);
 }
 // console.log(base)
 
 // console.log(new Date(1636146298918))
 
 function modInverse(e, phi) {
-    var m0 = phi, t, q;
-    var x0 = 0, x1 = 1;
+  var m0 = phi,
+    t,
+    q;
+  var x0 = 0,
+    x1 = 1;
 
-    if (phi == 1) {
-        return 0;
-    }
+  if (phi == 1) {
+    return 0;
+  }
 
-    console.log("e, phi, m0, x0, t, q, x1",e, phi, m0, x0, t, q, x1)
-    console.log("------------------------")
+  console.log("e, phi, m0, x0, t, q, x1", e, phi, m0, x0, t, q, x1);
+  console.log("------------------------");
 
-    while (e > 1) {
-        q = Math.floor(e / phi)
-        t = phi;
-        phi = e % phi, e = t;
-        t = x0
-        x0 = x1 - q * x0
-        x1 = t;
+  while (e > 1) {
+    q = Math.floor(e / phi);
+    t = phi;
+    (phi = e % phi), (e = t);
+    t = x0;
+    x0 = x1 - q * x0;
+    x1 = t;
 
-        console.log("e, phi, m0, x0, t, q, x1",e, phi, m0, x0, t, q, x1)
-    }
+    console.log("e, phi, m0, x0, t, q, x1", e, phi, m0, x0, t, q, x1);
+  }
 
-    if (x1 < 0) {
-        x1 += m0;
-        console.log(">>------------>")
-        console.log("e, phi, m0, x0, t, q, x1",e, phi, m0, x0, t, q, x1)
+  if (x1 < 0) {
+    x1 += m0;
+    console.log(">>------------>");
+    console.log("e, phi, m0, x0, t, q, x1", e, phi, m0, x0, t, q, x1);
+  }
 
-    }
-
-    return x1;
+  return x1;
 }
 // modInverse(7, 40);
 // console.log(Math.floor(3/10))
 
 function countDownToZero(n) {
-    // Base case. Stop at 0
-    if (n < 0) {
-        return; //Stop the function
-    } else {
-        console.log(n)
-        countDownToZero(n - 1); // Count down 1
-    }
+  // Base case. Stop at 0
+  if (n < 0) {
+    return; //Stop the function
+  } else {
+    console.log(n);
+    countDownToZero(n - 1); // Count down 1
+  }
 }
 
 // Iterative Solution
@@ -110,34 +112,87 @@ function countDownToZero(n) {
 // 3: last: sum = 1, lastlast = last = 1 sum = 2
 // countDownToZero(10)
 function getNthFibo(n) {
-    if (n <= 1) return n;
-    var sum = 0,
-        last = 1,
-        lastlast = 0;
+  if (n <= 1) return n;
+  var sum = 0,
+    last = 1,
+    lastlast = 0;
 
-    for(var i = 1; i < n; i++) {
-        sum = last + lastlast;
-        lastlast = last;
-        last = sum;
-    }
-    return sum;
+  for (var i = 1; i < n; i++) {
+    sum = last + lastlast;
+    lastlast = last;
+    last = sum;
+  }
+  return sum;
 }
 
-getNthFiboRec()
-getNthFiboRec(5) +  // getNthFiboRec(4) + getNthFiboRec(3) = 
-getNthFiboRec(4) +  // getNthFiboRec(3) + getNthFiboRec(2)
-getNthFiboRec(3) +  // getNthFiboRec(3) + getNthFiboRec(2)
-getNthFiboRec(2) +  // getNthFiboRec(2) + getNthFiboRec(1)
-getNthFiboRec(1) +  // getNthFiboRec(0)
-
+// getNthFiboRec()
+// getNthFiboRec(5) +  // getNthFiboRec(4) + getNthFiboRec(3) =
+// getNthFiboRec(4) +  // getNthFiboRec(3) + getNthFiboRec(2)
+// getNthFiboRec(3) +  // getNthFiboRec(3) + getNthFiboRec(2)
+// getNthFiboRec(2) +  // getNthFiboRec(2) + getNthFiboRec(1)
+// getNthFiboRec(1) +  // getNthFiboRec(0)
 
 // console.log(getNthFibo(6));
 function getNthFiboRec(n) {
-    if (n <= 1) {
-        return n;
-    }
-    // console.log(n);
-    // console.log(getNthFiboRec(n - 1) + getNthFiboRec(n-2))
-    return getNthFiboRec(n - 1) + getNthFiboRec(n - 2);
+  if (n <= 1) {
+    return n;
+  }
+  // console.log(n);
+  // console.log(getNthFiboRec(n - 1) + getNthFiboRec(n-2))
+  return getNthFiboRec(n - 1) + getNthFiboRec(n - 2);
 }
-console.log(getNthFiboRec(6))
+// console.log(getNthFiboRec(6))
+
+// Using two for loops
+// Time Complexity: O(n2)
+// Space Complexity: Object(1)
+function fundSum(arr, weight) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == weight) {
+        return [i, j];
+      }
+    }
+  }
+  return -1;
+}
+// console.log(fundSum([2,4,5,6,2], 9));
+
+/* 
+[1,2,3], 5
+
+i = 2
+differenct = 3
+
+0: currentElement = 1 difference = 4
+1: currentElement = 2 difference = 3
+2: currentElement = 3 difference = 2
+
+if the hashtable[currentElement] != undefined
+[i, hashtable[weight - difference]]
+
+{
+    4: 0
+    3: 1
+
+}
+*/
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+function findSumHash(arr, weight) {
+  var hashtable = {};
+  for (var i = 0, arrLength = arr.length; i < arrLength; i++) {
+    var currentElement = arr[i],
+      difference = weight - currentElement;
+
+    // Check if the right one already exists
+    if (hashtable[currentElement] != undefined) {
+      return [i, hashtable[weight - difference]];
+    } else {
+      // store index
+      hashtable[difference] = i;
+    }
+  }
+  return -1;
+}
+console.log(findSumHash([1, 3, 3,7, 4, 12, 2, 9, 2], 14));
