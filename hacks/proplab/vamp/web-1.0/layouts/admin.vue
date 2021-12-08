92c8b710ccb30877fee0fad2b727a5395c015c49
Damilola Faseun <damilola.faseun@gmail.com>
2021-12-08 21:03:42 +0100

    Admin layout

---
+++ hacks/proplab/vamp/web-1.0/layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <app-layout> -->
+      <div class="flex space-x-4">
+        <h1>Admin Layout</h1>
+        <div class="flex-1 ...">1</div>
+        <div class="flex-1 ...">2</div>
+        <div class="flex-1 ...">3</div>
+      </div>
+
+      <div>
+          <nuxt />
+      </div>
+    <!-- </app-layout> -->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    "app-layout": () => "@/components/layouts/app",
+  },
+};
+</script>
