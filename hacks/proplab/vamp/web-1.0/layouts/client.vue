c9da65883357b96a839d0af6aa0b0dc5b94b962f
Damilola Faseun <damilola.faseun@gmail.com>
2021-12-08 21:03:53 +0100

    client layout

---
+++ hacks/proplab/vamp/web-1.0/layouts/client.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-layout>
+      <div class="table w-full ...">
+        <div class="table-row-group">
+          <div class="table-row">
+            <div class="table-cell ...">A cell with more content</div>
+            <div class="table-cell ...">Cell 2</div>
+            <div class="table-cell ...">Cell 3</div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell ...">Cell 4</div>
+            <div class="table-cell ...">A cell with more content</div>
+            <div class="table-cell ...">Cell 6</div>
+          </div>
+
+          <nuxt />
+        </div>
+      </div>
+    </app-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    "app-layout": () => "@/components/layout/app",
+  },
+};
+</script>
