d9390665365c81e6fa390c6b0e5e056bb1e7959a
Damilola Faseun <damilola.faseun@gmail.com>
2022-01-01 00:13:48 +0100

    Reverse array second solution

--- hackerRank/reverse_array.js
+++ hackerRank/reverse_array.js
@@ -9,5 +9,16 @@ function reverseArray(array) {
   return array.map(() => array[length--]);
 }
 
+// initialize and return a new array
+const ReverseArray2 = (array) => {
+  const newArray = [];
+  let len = array.length;
+
+  while (len--) {
+    newArray.push(array[len]);
+  }
+
+  return newArray;
+};
 console.log(reverseArray([3, 2, 1]));
 console.log(ReverseArray2([1, 2, 3, 4, 5])); //[5,4,3,2,1]
