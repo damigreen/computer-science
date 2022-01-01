03e5246b3a296b8bb7a20b40e8868db175bc3e0a
Damilola Faseun <damilola.faseun@gmail.com>
2022-01-01 12:28:41 +0100

    Dynamic arrays prod

--- notes/vsnote/console.js
+++ notes/vsnote/console.js
@@ -248,7 +248,6 @@ function stackSearch(stack, element) {
   var bufferArray = stack.getBuffer();
   var bufferStack = new Stack(bufferArray);
   while (bufferStack.array.length > 0) {
-
     if (bufferStack.array.pop() == element) {
       return true;
     }
@@ -260,7 +259,6 @@ function stackSearch(stack, element) {
 // console.log(stackSearch(stack1, 0))
 // console.log(stackSearch(stack1, 3))
 
-
 /**
  * Queues
  */
@@ -269,56 +267,56 @@ function Queue(array) {
   if (array) this.array = array;
 }
 
-Queue.prototype.getBuffer = function() {
+Queue.prototype.getBuffer = function () {
   return this.array.slice();
-}
+};
 
-Queue.prototype.isEmpty = function() {
+Queue.prototype.isEmpty = function () {
   return this.array.length == 0;
-}
+};
 
 /**
  * Peek
  */
-Queue.prototype.peek = function() {
-  return this.array[0]
-}
+Queue.prototype.peek = function () {
+  return this.array[0];
+};
 
 // Insertion
 // O(1)
-Queue.prototype.enqueue = function(value) {
+Queue.prototype.enqueue = function (value) {
   return this.array.push(value);
-}
+};
 
 // Deletion
 // O(n)
-Queue.prototype.dequeue = function() {
+Queue.prototype.dequeue = function () {
   return this.array.shift();
-}
+};
 
 // Access
 // O(n)
 function queueAccessNthTopNode(queue, n) {
   var bufferArray = queue.getBuffer();
-  if (n <= 0) throw 'error';
+  if (n <= 0) throw "error";
 
   var bufferQueue = new Queue(bufferArray);
 
-  while(--n !== 0) {
-    bufferQueue.dequeue()
+  while (--n !== 0) {
+    bufferQueue.dequeue();
   }
-  return bufferQueue.dequeue()
+  return bufferQueue.dequeue();
 }
 
-var queue1 = new Queue([3,4,5,0,9]);
-queue1.enqueue(8)
-queue1.enqueue(9)
-queue1.enqueue(10)
+var queue1 = new Queue([3, 4, 5, 0, 9]);
+queue1.enqueue(8);
+queue1.enqueue(9);
+queue1.enqueue(10);
 // console.log(queue1)
-queue1.dequeue()
-queue1.dequeue()
-console.log(queue1)
-console.log(queueAccessNthTopNode(queue1, 1)) // 3
+queue1.dequeue();
+queue1.dequeue();
+console.log(queue1);
+console.log(queueAccessNthTopNode(queue1, 1)); // 3
 
 /**
  * Search
@@ -329,27 +327,99 @@ function queueSearch(queue, element) {
 
   var bufferQueue = new Queue(bufferArray);
 
-  while(!bufferQueue.isEmpty()) {
+  while (!bufferQueue.isEmpty()) {
     if (bufferQueue.dequeue() == element) return true;
   }
-  return false
+  return false;
 }
 // console.log(queueSearch(queue1, 10))
 // console.log(queueSearch(queue1, 2))
 
-
 /**
  * Dynamic Array
  * Query: 1 x y
-  * let idx = ((x (+) lastAnswer) % n)
-  * append integer y to idx
+ * let idx = ((x (+) lastAnswer) % n)
+ * append integer y to idx
  * Query: 2 x y
-  * let idx = ((x (+) lastAnswer) % n)
-  * assign the value arr[idx][y % size(arr[idx])] to lastAnswer
-  * store tne new value of lastAnswer to an answer array
- * 
+ * let idx = ((x (+) lastAnswer) % n)
+ * assign the value arr[idx][y % size(arr[idx])] to lastAnswer
+ * store tne new value of lastAnswer to an answer array
+ *
  */
 function dynamicArray(arr) {
-  let lastAnswer = 0
-  // let idx = 
+  let lastAnswer = 0;
+  // let idx =
 }
+
+/**
+ * Dynamic array products
+ *
+ * Given an array of integers, return an array
+ * products such that product[i] is equal to the
+ * product of all elements except arr[i].
+ * Solve without using the division operator
+ * in O(n) time
+ *
+ * arr = [1,2,3,4]
+ *     1        2        3        4
+ * [2*3*4*5, 1*3*4*5, 1*2*3*5, 1*2*3*4] -> [24,12,8,6]
+ *
+ * befors   -> [1,1,2,6]
+ * afters   -> [24,12,4,1]
+ * products -> [24,12,8,6]
+ * 
+ * Dynamic Arrays
+ * Automatically resizes
+ * Retrieving an element at a given index takes O(1) time
+ * Appends - If a dynamic array needs to expand to fit a
+ * new element, it copy all the existing element into a 
+ * second, bigger array, Hence an appends takes O(n)
+ * with each doubling, the n doubles as well,
+ * however with each doubling the number of O(1) appends
+ * doubles too. These two things cance each other out so we
+ * say appends have an amortized const of O(1)
+ *
+ */
+function dynamicArrayProd(arr) {
+  let beforeProductSoFar = 1;
+  const productsOfBeforeNumbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    // 1->1->2->6->24
+    // [1,1,2,6,24]
+    productsOfBeforeNumbers[i] = beforeProductSoFar;
+    beforeProductSoFar *= arr[i];
+  }
+
+  let afterProductSoFar = 1;
+  const productsOfAfterNumbers = [];
+  for (let j = arr.length - 1; j > -1; j--) {
+    productsOfAfterNumbers[j] = afterProductSoFar;
+    afterProductSoFar *= arr[j];
+  }
+
+  const products = [];
+  for (var k = 0; k < arr.length; k++) {
+    products[k] = productsOfAfterNumbers[k] * productsOfBeforeNumbers[k];
+  }
+
+  return products;
+}
+console.log(dynamicArrayProd([1, 2, 3, 4, 5]));
+
+function dynamicArrayProdRefactor(arr) {
+  let beforeProductSoFar = 1;
+  const products = [];
+  for (var i = 0; i < arr.length; i++) {
+    products[i] = beforeProductSoFar;
+    beforeProductSoFar *= arr[i];
+  }
+
+  let numbersAfterProductSoFar = 1;
+  for (let j = arr.length - 1; j > -1; j--) {
+    products[j] *= numbersAfterProductSoFar;
+    numbersAfterProductSoFar *= arr[j];
+  }
+
+  return products;
+}
+console.log(dynamicArrayProdRefactor([1, 2, 3, 4, 5]));
