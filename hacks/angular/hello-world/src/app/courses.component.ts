ef6323a3cea97fbbba1e4fa2be9ce4efeb4088db
Damilola Faseun <damilola.faseun@gmail.com>
2022-01-06 13:59:35 +0100

    Create course component

--- hacks/angular/hello-world/src/app/courses.component.ts
+++ hacks/angular/hello-world/src/app/courses.component.ts
@@ -1,8 +1,28 @@
+import { CoursesService } from './courses.service';
 import { Component } from '@angular/core';
 
 @Component({
     selector: "courses",
-    template: "<h2>Good God</h2>"
+    template:   `<h2>{{"Title:" + title }}</h2>
+                    <p>{{ getTitle() }}</p>
+                    <ul>
+                    <li *ngFor="let course of courses">
+                        {{ course }}
+                    </li>
+                    </ul>
+                `
 })
 
-export class CoursesComponent {}
+export class CoursesComponent {
+    title = "Course Description";
+    // courses = ["Course1","Course2","Course3","Course4"]
+    courses;
+    constructor() {
+        let service = new CoursesService();
+        this.courses = service.getCourses()
+    }
+
+    getTitle() {
+        return this.title.length;
+    }
+}
