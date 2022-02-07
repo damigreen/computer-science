6cbbd85d16a8d52e4f3322f5704a350a4f42dc60
Damilola Faseun <damilola.faseun@gmail.com>
2022-02-07 11:25:44 +0100

    Hackerank: linked list insertion at head

--- CS/hackerRank/linked_list_insert_at_head.js
+++ CS/hackerRank/linked_list_insert_at_head.js
@@ -7,17 +7,18 @@ function SinglyLinkedList() {
   this.head = null;
   this.size = 0;
 }
-
 /**
  * Time complexity O(1)
  */
 SinglyLinkedList.prototype.insert = function (value) {
   if (this.head == null) {
     this.head = new SinglyLinkedListNode(value);
+    this.size++
   } else {
     var temp = this.head;
     this.head = new SinglyLinkedListNode(value);
     this.head.next = temp;
+    this.size++;
   }
   this.size++;
 };
