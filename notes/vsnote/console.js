464d3b2b6f9a0df12f92c4d35e0b6b3935fdcf97
Damilola Faseun <damilola.faseun@gmail.com>
2022-02-07 11:24:32 +0100

    new ppm date logic

---
+++ notes/vsnote/console.js
@@ -0,0 +1,10 @@
+var date = new Date(1644662144000)
+var date1 = new Date("2022-02-04T10:35:44.000Z");
+var date2 = new Date(1643970944000);
+var date3 = new Date("2022-02-04T20:48:00.000Z").getTime();
+var date4 = new Date(1628985600000);
+console.log(date)
+console.log(date1.getTime())
+console.log(date2)
+console.log(date3)
+console.log(date4)
