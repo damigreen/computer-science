16ec691c777362a4e56aea7dbad63343835149ed
Damilola Faseun <damilola.faseun@gmail.com>
2022-02-20 16:08:49 +0100

    New pets instances

--- CS/books/Head First Java/Dog.java
+++ CS/books/Head First Java/Dog.java
@@ -20,5 +20,30 @@ class DogTestDrive {
         String name = d.sayName();
         System.out.println(name);
         d.bark();
+        Dog[] pets;
+        pets = new Dog[7];
+
+        pets[0] = new Dog();
+        pets[0].size = 12;
+        pets[0].breed = "German Shepard";
+        System.out.println(pets[0].breed);
+
+
+        pets[1] = new Dog();
+        pets[1].size = 13;
+        System.out.println(pets[1].size);
+
+        pets[3] = d;
+        pets[3].bark();
+
+        System.out.println("last dogs name is ");
+        System.out.println(pets[3].name);
+
+        int x = 0;
+        while(x < pets.length) {
+            System.out.println(pets.length);
+            pets[x].bark();
+            x = x + 1;
+        }
     }
 }
