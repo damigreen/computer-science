00de22b2e5cabcdffb9dd8594962ff8b7c736070
Damilola Faseun <damilola.faseun@gmail.com>
2022-03-12 17:52:36 +0100

    Simple dot com logic

--- CS/books/Head First Java/SimpleDotCom.java
+++ CS/books/Head First Java/SimpleDotCom.java
@@ -8,6 +8,8 @@ public class SimpleDotCom {
 
     public String checkYourself(String stringGuess) {
         int guess = Integer.parseInt(stringGuess);
+        System.out.println("guess____");
+        System.out.println(guess);
         String result = "miss";
 
         for (int cell : locationCells) {
@@ -22,5 +24,28 @@ public class SimpleDotCom {
         }
         System.out.println(result);
         return result;
+    }
+
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("enter a number!");
+
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numOfGuesses + " guesses");
+            }
+        }
     }
 }
