0ca5a3572d0918b653bc5658ee89fbad6e9cc0ac
Damilola Faseun <damilola.faseun@gmail.com>
2022-03-24 22:43:04 +0100

    Publish logic iot landwey

--- hacks/mqtt/mqtt-example/pub.js
+++ hacks/mqtt/mqtt-example/pub.js
@@ -1,13 +1,38 @@
 const mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://broker.hivemq.com");
+// var client = mqtt.connect("mqtt://broker.hivemq.com");
+
+var options = {
+  host: "5535608506a44d8dbecdbc5eaed7bd52.s1.eu.hivemq.cloud",
+  port: 8883,
+  protocol: "mqtts",
+  username: "landwey-iot",
+  password: "bU3d@Y@apGzNEtu",
+};
+
+var client = mqtt.connect(options);
 
 client.on("connect", function () {
-  setInterval(function () {
+  console.log("connected");
+  // setInterval(function () {
     var random = Math.random() * 50;
     console.log(random);
-    if (random < 30) {
-      client.publish("Damigreen", "God is good always!!" + random.toString());
-    }
-  }),
-    300000;
+    // if (random < 30) {
+      // client.publish("Damigreen", "God is good always!!" + random.toString());
+      client.publish("Energy Reading", "{ Energy: '100MW', time: 9 }", { qos: 1 });
+      client.publish(
+        "Generator Metrics",
+        "{ Energy: '130MW', time: 7 }"
+      , { qos: 1 });
+      client.publish("Fuel Reading", "{ Energy: '290MW', time: 9 }", { qos: 1 });
+      client.publish(
+        "Water Flow - Phase One",
+        "{ Energy: '2000MW', time: 3 }"
+      , { qos: 1 });
+      client.publish(
+        "Water Flow - Phase Two",
+        "{ Energy: '900MW', time: 42 }"
+      , { qos: 1 });
+      console.log("published!!")
+    // }
+  // }, 3000);
 });
