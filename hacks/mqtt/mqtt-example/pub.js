3e4a425b1112f9514d0ca69ed834177cf28fcf1a
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-07 20:49:12 +0100

    Mqtt publish logic

--- hacks/mqtt/mqtt-example/pub.js
+++ hacks/mqtt/mqtt-example/pub.js
@@ -1,5 +1,4 @@
 const mqtt = require("mqtt");
-// var client = mqtt.connect("mqtt://broker.hivemq.com");
 
 var options = {
   // host: "x5f2dc2d.us-east-1.emqx.cloud",
@@ -19,33 +18,15 @@ var options = {
   // password: "bU3d@Y@apGzNEtu",
 };
 
-var client = mqtt.connect(options);
+// var client = mqtt.connect(options);
+var client = mqtt.connect("mqtt://broker.hivemq.com");
 
 client.on("connect", function () {
   console.log("connected");
-
-  // setInterval(function () {
-    var random = Math.random() * 50;
-    console.log(random);
-    client.publish('Energy Lendway', '15')
-    client.publish('Fuel Monitoring Lendway', '45')
-    // if (random < 30) {
-      // client.publish("Damigreen", "God is good always!!" + random.toString());
-      // client.publish("Energy Reading", "{ Energy: '100MW', time: 9 }", { qos: 1 });
-      // client.publish(
-      //   "Generator Metrics",
-      //   "{ Energy: '130MW', time: 7 }"
-      // , { qos: 1 });
-      // client.publish("Fuel Reading", "{ Energy: '290MW', time: 9 }", { qos: 1 });
-      // client.publish(
-      //   "Water Flow - Phase One",
-      //   "{ Energy: '2000MW', time: 3 }"
-      // , { qos: 1 });
-      // client.publish(
-      //   "Water Flow - Phase Two",
-      //   "{ Energy: '900MW', time: 42 }"
-      // , { qos: 1 });
-      // console.log("published!!")
-    // }
-  // }, 3000);
+  setInterval(() => {
+    console.log(Math.random());
+    client.publish("damigreen", "God is good! do not break fasting", {
+      qos: 2,
+    });
+  }, 3000);
 });
