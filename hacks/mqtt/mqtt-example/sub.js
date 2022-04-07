b33106a63e91ddf8e4b33e8211f4cb54ff0e63d3
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-07 20:49:26 +0100

    Mqtt subscription logic

--- hacks/mqtt/mqtt-example/sub.js
+++ hacks/mqtt/mqtt-example/sub.js
@@ -2,8 +2,8 @@ const mqtt = require("mqtt");
 var client = mqtt.connect("mqtt://broker.hivemq.com");
 
 client.on('connect', function() {
-    client.subscribe("Damigreen");
-    console.log("Client has subscribed successfully!")
+    client.subscribe("damigreen");
+    console.log("Client has subscribed successfully!", { qos: 2 })
 })
 client.on('message', function(topic,message) {
     console.log("topic " + topic);
