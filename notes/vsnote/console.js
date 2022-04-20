627b619febfb6600b828c56a9d47a5c1c43b9c5c
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-20 11:26:29 +0100

    New date

--- notes/vsnote/console.js
+++ notes/vsnote/console.js
@@ -534,3 +534,19 @@ function formatAMPM(date = new Date()) { // This is to display 12 hour format li
 var myDate = new Date();
 var displayDate = myDate.getMonth()+ '/' +myDate.getDate()+ '/' +myDate.getFullYear()+ ' ' +formatAMPM(myDate);
 console.log(displayDate);
+
+function formatDate(date = new Date()) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
+
+var de = new Date();
+var e = formatDate(de);
+
+console.log(e);
