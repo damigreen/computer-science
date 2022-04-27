6a7ca7197efcb6a415daa4552af16bf308cebd00
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-27 12:03:27 +0100

    Installed experess

--- courses/NodeJS - The Complet Guide/server.js
+++ courses/NodeJS - The Complet Guide/server.js
@@ -1,5 +1,14 @@
 const http = require("http");
+const express = require('express')
+const app = express();
+app.use((req, res, next) => {
+    console.log("middleware");
+    next(); // Allow request to continue to the next middlewate in line
+})
+app.use((req, res, next) => {
+    console.log("middleware next");
+})
 const routes = require("./routes");
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
