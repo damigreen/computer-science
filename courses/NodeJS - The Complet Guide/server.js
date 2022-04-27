88c17dfd3c4f50aa5f6c79f949db16c3f236407a
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-27 13:03:31 +0100

    Reconfigured server

--- courses/NodeJS - The Complet Guide/server.js
+++ courses/NodeJS - The Complet Guide/server.js
@@ -1,14 +1,24 @@
 const http = require("http");
-const express = require('express')
+const express = require("express");
 const app = express();
-app.use((req, res, next) => {
-    console.log("middleware");
-    next(); // Allow request to continue to the next middlewate in line
-})
-app.use((req, res, next) => {
-    console.log("middleware next");
-})
-const routes = require("./routes");
-const server = http.createServer(app);
 
-server.listen(3000);
+// app.use((req, res, next) => {
+//   console.log("middleware");
+//   next(); // Allow request to continue to the next middlewate in line
+// });
+
+app.use("/add-products", (req, res, next) => {
+  console.log("middleware next");
+  res.send("<h1>Add products to cart</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("middleware next");
+  res.send("<h1>Hello from express</h1>");
+});
+
+
+// const routes = require("./routes");
+// const server = http.createServer(routes );
+
+app.listen(3000);
