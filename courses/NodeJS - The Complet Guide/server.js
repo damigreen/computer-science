fb739ff1fc98c8fdc7a2aa01bad561a1c0965b1b
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-30 16:31:04 +0100

    Use get and post method

--- courses/NodeJS - The Complet Guide/server.js
+++ courses/NodeJS - The Complet Guide/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
 // app.use((req, res, next) => {
 //   console.log("middleware");
@@ -8,16 +9,26 @@ const app = express();
 //   next(); // Allow request to continue to the next middlewate in line
 // });
 
-app.use("/add-products", (req, res, next) => {
+app.use(bodyParser.urlencoded())
+
+app.use("/add-product", (req, res, next) => {
   console.log("middleware next");
-  res.send("<h1>Add products to cart</h1>");
+  res.send(
+    `<h1>Add products to cart</h1>
+    <form action='/product' method='POST'><input type='text' name='title'></input><button type='submit'>Add product</button></form>
+    `
+  );
+});
+
+app.post("/product", (req, res, next) => {  
+  console.log(req.body);
+  res.redirect('/')
 });
 
 app.use("/", (req, res, next) => {
   console.log("middleware next");
   res.send("<h1>Hello from express</h1>");
 });
-
 
 // const routes = require("./routes");
 // const server = http.createServer(routes );
