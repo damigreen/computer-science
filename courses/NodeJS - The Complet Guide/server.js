e14ace60517abddc712a7e748584717301f9bd61
Damilola Faseun <damilola.faseun@gmail.com>
2022-04-30 18:40:35 +0100

    Wire server file

--- courses/NodeJS - The Complet Guide/server.js
+++ courses/NodeJS - The Complet Guide/server.js
@@ -1,17 +1,26 @@
 const http = require("http");
+const path = require("path");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
+const middleware = require("./middleware/request");
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.send({ message: "God is good" });
+});
+
+app.use("/admin", adminRouter);
 app.use(shopRouter);
-app.use(adminRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
+app.use(middleware.unknownEndpoint);
 
 app.listen(3000);
