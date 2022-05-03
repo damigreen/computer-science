e2e92d20b4f87087cd63d84ac9c1641a21f71024
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-03 09:20:18 +0100

    Shop logic

--- courses/NodeJS - The Complet Guide/routes/shop.js
+++ courses/NodeJS - The Complet Guide/routes/shop.js
@@ -2,12 +2,20 @@ const express = require("express");
 const path = require("path");
 
 const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get("/shop", (req, res, next) => {
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  
+  console.log("products >>--------------->")
+  console.log(adminData.products)
+  // console.log(products)
+  res.render("shop", { prods: products, docTitle: "Shop" });
+  // res.send("God is good");
   //   res.sendFile(path.join(__dirname, '../', "views", "shop.html"));
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
