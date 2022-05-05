8569d5e0692995864d8fc25145abd31aafe8bf10
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-05 15:32:07 +0100

    Render 404.pug

--- courses/NodeJS - The Complet Guide/server.js
+++ courses/NodeJS - The Complet Guide/server.js
@@ -16,9 +16,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRouter.routes);
 app.use(shopRouter);
 
-
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404');
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.use(middleware.unknownEndpoint);
