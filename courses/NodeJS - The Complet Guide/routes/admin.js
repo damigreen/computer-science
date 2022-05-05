995e1eba2fa58f8411e51d0701ba306f7ec09d8d
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-05 15:32:22 +0100

    Change export format

--- courses/NodeJS - The Complet Guide/routes/admin.js
+++ courses/NodeJS - The Complet Guide/routes/admin.js
@@ -8,7 +8,8 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", {pageTiele: "Add Product"});
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
