43816d80b866f165c2f5b5c4d67d4502d58467e5
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-05 12:51:33 +0100

    Console element

--- notes/vsnote/console.js
+++ notes/vsnote/console.js
@@ -8,19 +8,12 @@ function BinaryTree() {
   this._root = null;
 }
 
-console.log(new Date(1651567143359 + 10226343358.999968));
-console.log(new Date(1641945600000));
-// 1641945600.00
-console.log(new Date("2022, 05, 06").getTime());
-// data_now, cycle_due, time_to_due, due_at >>------------->
-// 1651567996.54 1654038016 2470019.460000038 1649702777.08
-// console.log(new Date(1649702777000));
-// console.log(new Date(1651567996540 - 2470019.460000038));
-
-// data_now, cycle_due, time_to_due, due_at >>------------->
-// 1651568658.362 1651791616 222957.63800001144 1651950500.724
-
-console.log(new Date(1651950500724))
-console.log(new Date(1653160064000));
-console.log(new Date(1653160064000));
-console.log(new Date("2022, 05, 02").getTime());
+BinaryTree.prototype.traversePreOrder = function() {
+  traversePreOrderHelper(this._root)
+  function traversePreOrderHelper(node) {
+    if (!node) return;
+    console.log(node.value);
+    traversePreOrderHelper(node.left)
+    traversePreOrderHelper(node.right)
+  }
+}
