b09ab3237866d4eeee7ac41585964e0e815de44b
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-05 12:51:21 +0100

    Shop template

--- courses/NodeJS - The Complet Guide/routes/shop.js
+++ courses/NodeJS - The Complet Guide/routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  const products = adminData.products;
+    const products = adminData.products;
   
   console.log("products >>--------------->")
   console.log(adminData.products)
